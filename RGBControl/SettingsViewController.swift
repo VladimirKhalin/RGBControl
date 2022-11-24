7cda7bb16306135586041995dc57b61e1e2d7204
VK <art.nebar@mail.ru>
2022-11-24 21:11:13 +0300

    Relize

--- RGBControl/SettingsViewController.swift
+++ RGBControl/SettingsViewController.swift
@@ -21,7 +21,9 @@ final class SettingsViewController: UIViewController {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
-    @IBOutlet var textFieldsRGBControl: [UITextField]!
+    @IBOutlet var redTextField: UITextField!
+    @IBOutlet var greenTextField: UITextField!
+    @IBOutlet var blueTextField: UITextField!
     
     @IBOutlet var colorPalitraView: UIView!
     
@@ -37,9 +39,9 @@ final class SettingsViewController: UIViewController {
         greenSlider.minimumTrackTintColor = .green
         blueSlider.minimumTrackTintColor = .blue
         
-        textFieldsRGBControl[0].delegate = self
-        textFieldsRGBControl[1].delegate = self
-        textFieldsRGBControl[2].delegate = self
+        redTextField.delegate = self
+        greenTextField.delegate = self
+        blueTextField.delegate = self
         
         let colorsPosition = UIColorSeparated(color: RGBsettings)
         
@@ -49,40 +51,29 @@ final class SettingsViewController: UIViewController {
         blueSlider.value = Float(colorsPosition[2])
         
         // В label показываем значения положениея слайдереров
-        redLabel.text = String(format: "%.2f", redSlider.value)
-        greenLabel.text = String(format: "%.2f", greenSlider.value)
-        blueLabel.text = String(format: "%.2f", blueSlider.value)
+        transferValueSliderToLable()
         
         // Присваиваем textField значениям label
-        for textField in textFieldsRGBControl {
-            switch textField.tag {
-            case 0: textField.text = redLabel.text
-            case 1: textField.text = greenLabel.text
-            default: textField.text = blueLabel.text
-            }
-        }
+        transferValueLableTotextFild()
+        
         // Красим политру в цвет первого экрана
         self.colorPalitraView.backgroundColor = RGBsettings
+        
+        addToolBar()
+    }
+    @objc func tapDone() {
+        self.view.endEditing(true)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
     }
     
     @IBAction func sliderActions(_ sender: UISlider) {
-        redLabel.text = String(format: "%.2f", redSlider.value)
-        greenLabel.text = String(format: "%.2f", greenSlider.value)
-        blueLabel.text = String(format: "%.2f", blueSlider.value)
-        
-        for textField in textFieldsRGBControl {
-            switch textField.tag {
-            case 0: textField.text = redLabel.text
-            case 1: textField.text = greenLabel.text
-            default: textField.text = blueLabel.text
-            }
-        }
-        
-        self.colorPalitraView.backgroundColor = UIColor(
-            red: CGFloat(redSlider.value),
-            green: CGFloat(greenSlider.value),
-            blue: CGFloat(blueSlider.value),
-            alpha: 1.0)
+        transferValueSliderToLable()
+        transferValueLableTotextFild()
+        self.colorPalitraView.backgroundColor = makeUIColor()
     }
     
     @IBAction func doneButtonPressed() {
@@ -90,6 +81,41 @@ final class SettingsViewController: UIViewController {
         RGBsettings = colorPalitraView.backgroundColor
         delegate?.setColor(color: RGBsettings)
         dismiss(animated: true)
+    }
+    
+    private func addToolBar() {
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        let flex = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let barButton = UIBarButtonItem(
+            title: "Done",
+            style: .done,
+            target: self,
+            action: #selector(tapDone))
+        toolBar.items = [flex, barButton]
+        redTextField.inputAccessoryView = toolBar
+        greenTextField.inputAccessoryView = toolBar
+        blueTextField.inputAccessoryView = toolBar
+    }
+    
+    private func makeUIColor() -> UIColor {
+        UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1.0)
+    }
+    
+    private func transferValueLableTotextFild() {
+        redTextField.text = redLabel.text
+        greenTextField.text = greenLabel.text
+        blueTextField.text = blueLabel.text
+    }
+    
+    private func transferValueSliderToLable() {
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
     }
     
     private func UIColorSeparated(color: UIColor) -> [CGFloat] {
@@ -108,13 +134,8 @@ extension SettingsViewController: UITextFieldDelegate {
         case 1: greenSlider.value = Float(result) ?? 0.0
         default: blueSlider.value = Float(result) ?? 0.0
         }
-        
-        let newUIColor = UIColor(
-            red: CGFloat(redSlider.value),
-            green: CGFloat(greenSlider.value),
-            blue: CGFloat(blueSlider.value),
-            alpha: 1
-        )
+        let newUIColor = makeUIColor()
         colorPalitraView.backgroundColor = newUIColor
     }
 }
+
